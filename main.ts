655f5cf07f2a8fba9a845106e1caac1e98a427d1
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-04-10 18:16:07 +0100

    button release not working have to use STOP

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+function REV_ALL () {
+	
+}
 function REV_Left () {
     pins.analogWritePin(AnalogPin.P1, 150)
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
 }
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.StickFigure)
     bluetooth.startUartService()
+    basic.showIcon(IconNames.StickFigure)
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
@@ -33,10 +36,10 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
     bluetooth.uartWriteString(cmd)
     if (cmd == "Forward") {
-    	
+        Forward_ALL()
     }
     if (cmd == "Reverse") {
-    	
+        ALL_STOP()
     }
     if (cmd == "Right") {
     	
@@ -46,6 +49,9 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
     }
     if (cmd == "stop") {
     	
+    }
+    if (cmd == "") {
+        ALL_STOP()
     }
 })
 // PIN14 = Controller on(1) Off (0)
@@ -82,8 +88,7 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
 // 
 // Speed PWM: P2
 input.onButtonPressed(Button.AB, function () {
-    REV_Left()
-    RWD_Right()
+    Forward_ALL()
     basic.pause(1000)
     ALL_STOP()
 })
@@ -92,12 +97,22 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     ALL_STOP()
 })
+function Forward_ALL () {
+    pins.analogWritePin(AnalogPin.P1, 256)
+    pins.analogWritePin(AnalogPin.P2, 256)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
 function RWD_Right () {
     pins.analogWritePin(AnalogPin.P2, 150)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 let cmd = ""
-basic.showIcon(IconNames.LeftTriangle)
+cmd = ""
+basic.showIcon(IconNames.Target)
 basic.pause(100)
 pins.digitalWritePin(DigitalPin.P14, 1)
+ALL_STOP()
