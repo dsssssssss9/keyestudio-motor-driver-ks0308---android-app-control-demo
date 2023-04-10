0694d78a4be6ea0b4a67ad16b77fec749e0689f8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-04-10 20:49:34 +0100

    basic app now works

--- main.ts
+++ main.ts
@@ -11,64 +11,17 @@ function REV_Left () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
 }
-bluetooth.onBluetoothConnected(function () {
-    bluetooth.startUartService()
-    basic.showIcon(IconNames.StickFigure)
-})
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
-input.onButtonPressed(Button.A, function () {
-    FWD_Left()
-    basic.pause(500)
-    ALL_STOP()
-})
-function SPIN_Right () {
-    FWD_Left()
-    REV_Right()
-}
-function ALL_STOP () {
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
-}
-function REV_Right () {
-    pins.analogWritePin(AnalogPin.P2, 150)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function FWD_Right () {
-    pins.analogWritePin(AnalogPin.P2, 150)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-}
-function SPIN_Left () {
+function ROT_Left () {
     FWD_Right()
     REV_Left()
 }
-function FWD_Left () {
-    pins.analogWritePin(AnalogPin.P1, 150)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P12, 0)
+function ROT_Right () {
+    FWD_Left()
+    REV_Right()
 }
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
-    cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
-    bluetooth.uartWriteString(cmd)
-    if (cmd == "Forward") {
-        Forward_ALL()
-    }
-    if (cmd == "Reverse") {
-        REV_ALL()
-    }
-    if (cmd == "Right") {
-        FWD_Left()
-    }
-    if (cmd == "Left") {
-        FWD_Right()
-    }
-    if (cmd == "stop") {
-        ALL_STOP()
-    }
-})
 // PIN14 = Controller on(1) Off (0)
 // 
 // Motor1 forward:
@@ -107,6 +60,55 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(1000)
     ALL_STOP()
 })
+input.onButtonPressed(Button.A, function () {
+    FWD_Left()
+    basic.pause(500)
+    ALL_STOP()
+})
+function ALL_STOP () {
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+function REV_Right () {
+    pins.analogWritePin(AnalogPin.P2, 150)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function FWD_Right () {
+    pins.analogWritePin(AnalogPin.P2, 150)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+}
+function FWD_Left () {
+    pins.analogWritePin(AnalogPin.P1, 150)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+    bluetooth.uartWriteString(cmd)
+    if (cmd == "Forward") {
+        Forward_ALL()
+    }
+    if (cmd == "Reverse") {
+        REV_ALL()
+    }
+    if (cmd == "Right") {
+        FWD_Left()
+    }
+    if (cmd == "Left") {
+        FWD_Right()
+    }
+    if (cmd == "ROT_Left") {
+        ROT_Left()
+    }
+    if (cmd == "ROT_Right") {
+        ROT_Right()
+    }
+    if (cmd == "stop") {
+        ALL_STOP()
+    }
+})
 input.onButtonPressed(Button.B, function () {
     FWD_Right()
     basic.pause(500)
@@ -120,6 +122,10 @@ function Forward_ALL () {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P12, 0)
 }
+bluetooth.onBluetoothConnected(function () {
+    bluetooth.startUartService()
+    basic.showIcon(IconNames.StickFigure)
+})
 let cmd = ""
 cmd = ""
 basic.showIcon(IconNames.Angry)
