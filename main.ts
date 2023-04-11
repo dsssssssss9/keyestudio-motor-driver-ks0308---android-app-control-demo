ce8460d7555a351817f430b22325e6e5cf1d9e10
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-04-11 10:15:01 +0100

    Defined speed variable in ONSTART

--- main.ts
+++ main.ts
@@ -1,27 +1,80 @@
 function REV_ALL () {
-    pins.analogWritePin(AnalogPin.P1, 150)
-    pins.analogWritePin(AnalogPin.P2, 150)
+    pins.analogWritePin(AnalogPin.P1, Speed)
+    pins.analogWritePin(AnalogPin.P2, Speed)
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 function REV_Left () {
-    pins.analogWritePin(AnalogPin.P1, 150)
+    pins.analogWritePin(AnalogPin.P1, Speed)
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
 }
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.Sad)
-})
 function ROT_Left () {
     FWD_Right()
     REV_Left()
 }
+bluetooth.onBluetoothConnected(function () {
+    bluetooth.startUartService()
+    basic.showIcon(IconNames.StickFigure)
+})
 function ROT_Right () {
     FWD_Left()
     REV_Right()
 }
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.A, function () {
+    FWD_Left()
+    basic.pause(500)
+    ALL_STOP()
+})
+function ALL_STOP () {
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+}
+function REV_Right () {
+    pins.analogWritePin(AnalogPin.P2, Speed)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function FWD_Right () {
+    pins.analogWritePin(AnalogPin.P2, Speed)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+}
+function FWD_Left () {
+    pins.analogWritePin(AnalogPin.P1, Speed)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+    bluetooth.uartWriteString(cmd)
+    if (cmd == "Forward") {
+        Forward_ALL()
+    }
+    if (cmd == "Reverse") {
+        REV_ALL()
+    }
+    if (cmd == "Right") {
+        FWD_Left()
+    }
+    if (cmd == "Left") {
+        FWD_Right()
+    }
+    if (cmd == "ROT_Left") {
+        ROT_Left()
+    }
+    if (cmd == "ROT_Right") {
+        ROT_Right()
+    }
+    if (cmd == "stop") {
+        ALL_STOP()
+    }
+})
 // PIN14 = Controller on(1) Off (0)
 // 
 // Motor1 forward:
@@ -60,75 +113,24 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(1000)
     ALL_STOP()
 })
-input.onButtonPressed(Button.A, function () {
-    FWD_Left()
-    basic.pause(500)
-    ALL_STOP()
-})
-function ALL_STOP () {
-    pins.analogWritePin(AnalogPin.P1, 0)
-    pins.analogWritePin(AnalogPin.P2, 0)
-}
-function REV_Right () {
-    pins.analogWritePin(AnalogPin.P2, 150)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function FWD_Right () {
-    pins.analogWritePin(AnalogPin.P2, 150)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    pins.digitalWritePin(DigitalPin.P15, 0)
-}
-function FWD_Left () {
-    pins.analogWritePin(AnalogPin.P1, 150)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P12, 0)
-}
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
-    cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
-    bluetooth.uartWriteString(cmd)
-    if (cmd == "Forward") {
-        Forward_ALL()
-    }
-    if (cmd == "Reverse") {
-        REV_ALL()
-    }
-    if (cmd == "Right") {
-        FWD_Left()
-    }
-    if (cmd == "Left") {
-        FWD_Right()
-    }
-    if (cmd == "ROT_Left") {
-        ROT_Left()
-    }
-    if (cmd == "ROT_Right") {
-        ROT_Right()
-    }
-    if (cmd == "stop") {
-        ALL_STOP()
-    }
-})
 input.onButtonPressed(Button.B, function () {
     FWD_Right()
     basic.pause(500)
     ALL_STOP()
 })
 function Forward_ALL () {
-    pins.analogWritePin(AnalogPin.P1, 256)
-    pins.analogWritePin(AnalogPin.P2, 256)
+    pins.analogWritePin(AnalogPin.P1, Speed)
+    pins.analogWritePin(AnalogPin.P2, Speed)
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P12, 0)
 }
-bluetooth.onBluetoothConnected(function () {
-    bluetooth.startUartService()
-    basic.showIcon(IconNames.StickFigure)
-})
+let Speed = 0
 let cmd = ""
 cmd = ""
-basic.showIcon(IconNames.Angry)
+Speed = 256
+basic.showIcon(IconNames.Ghost)
 basic.pause(100)
 pins.digitalWritePin(DigitalPin.P14, 1)
 ALL_STOP()
